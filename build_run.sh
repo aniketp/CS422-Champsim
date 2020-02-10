6c0657b29a05328459933808cdc416cc445182fc
shubhsherl <shubham.singh094@yahoo.com>
2020-02-10 18:57:24 +0530

    modify build_run script

--- build_run.sh
+++ build_run.sh
@@ -2,9 +2,11 @@
 
 TRACES=(  "641.leela_s-1052B.champsimtrace.xz" "641.leela_s-1083B.champsimtrace.xz" "641.leela_s-149B.champsimtrace.xz" "641.leela_s-334B.champsimtrace.xz" "641.leela_s-602B.champsimtrace.xz" "641.leela_s-800B.champsimtrace.xz" "641.leela_s-862B.champsimtrace.xz")
 
+BINARY="group2"
+
 if [ "$#" -lt 2 ]; then
     echo "Illegal number of parameters"
-    echo "Usage: ./build_run.sh [NUMBER] [BUILD]"
+    echo "Usage: ./build_run.sh [NUMBER] [BUILD] [predictor(optional) default:group2]"
     echo "NUMBER: 0-8, BUILD: 0(run) or 1(build and run)"
     echo "0: run all 7 traces"
     for i in $(seq 1 7)
@@ -15,38 +17,38 @@ if [ "$#" -lt 2 ]; then
     exit 1
 fi
 
+if [ ! -z "${3}" ]
+  then
+    BINARY=${3}
+fi
+
 NUM=${1}
 BUILD=${2}
-TRACE="ALL"
 TRACE_DIR=$PWD/dpc3_traces
-BINARY="group2-no-no-no-no-lru-1core"
+BINARY_FILE="$BINARY-no-no-no-no-lru-1core"
 N_WARM=50
 N_SIM=200
-RESULT="result_$(date +%s).txt"
+RESULT="result_${BINARY}_$(date +%s).txt"
 
 case $BUILD in
     0) 
-        echo "using old binary file"
+        echo "using old $BINARY file"
       ;;
     1)
-        echo "building new binary file..."
-        ./build_champsim.sh group2 no no no no lru 1
+        echo "building new $BINARY file..."
+        ./build_champsim.sh $BINARY no no no no lru 1
       ;;
     *)
         echo "[ERROR] Invalid BUILD number"
         exit 1
 esac
 
-if [ -f "result.txt" ] ; then
-    rm result.txt
-fi
-
 run() {
     echo "running trace: $1"
-    ./run_champsim.sh ${BINARY} ${N_WARM} ${N_SIM} ${1}
+    ./run_champsim.sh ${BINARY_FILE} ${N_WARM} ${N_SIM} ${1}
     echo ${1} >> $RESULT
-    (cat results_${N_SIM}M/${1}-group2-no-no-no-no-lru-1core.txt | grep "CPU 0 cumulative IPC:\|CPU 0 Branch Prediction Accuracy:" ) >> $RESULT
-    (cat results_${N_SIM}M/${1}-group2-no-no-no-no-lru-1core.txt | grep -A 8 "Branch types" ) >> $RESULT
+    (cat results_${N_SIM}M/${1}-$BINARY-no-no-no-no-lru-1core.txt | grep "CPU 0 cumulative IPC:\|CPU 0 Branch Prediction Accuracy:" ) >> $RESULT
+    (cat results_${N_SIM}M/${1}-$BINARY-no-no-no-no-lru-1core.txt | grep -A 8 "Branch types" ) >> $RESULT
     echo "---------------------------------------" >> $RESULT
 }
 
